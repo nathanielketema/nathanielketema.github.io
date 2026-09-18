f4c5bfeb568db5d4d3bcdb17402f353d332f1f4c
Nathaniel Ketema <nathanielketema@gmail.com>
2026-09-17 19:58:41 -0500

    add tools for better tooling

--- build.zig
+++ build.zig
@@ -2,16 +2,10 @@ const std = @import("std");
 const Website = @import("src/website.zig").Website;
 
 pub fn build(b: *std.Build) void {
-    const io = b.graph.io;
-
     const pandoc = get_pandoc_bin(b) orelse return;
     const website = Website.init(b, pandoc);
     website.build();
 
-    b.build_root.handle.deleteTree(io, "zig-out") catch |err| {
-        std.debug.print("unable to delete `zig-out`: {t}\n", .{err});
-        std.process.exit(1);
-    };
     const content_install = b.addInstallDirectory(.{
         .source_dir = website.content.getDirectory(),
         .install_dir = .prefix,
@@ -19,6 +13,23 @@ pub fn build(b: *std.Build) void {
         .exclude_extensions = &.{".DS_Store"},
     });
     b.getInstallStep().dependOn(&content_install.step);
+
+    const tools_exe = b.addExecutable(.{
+        .name = "tools",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/tools.zig"),
+            .target = b.standardTargetOptions(.{}),
+            .optimize = b.standardOptimizeOption(.{}),
+        }),
+    });
+    b.installArtifact(tools_exe);
+
+    const tools_step = b.step("tools", "Tools to make life easier.");
+
+    const tools_cmd = b.addRunArtifact(tools_exe);
+    if (b.args) |args| tools_cmd.addArgs(args);
+    tools_step.dependOn(&tools_cmd.step);
+    tools_cmd.step.dependOn(b.getInstallStep());
 }
 
 fn get_pandoc_bin(b: *std.Build) ?std.Build.LazyPath {
